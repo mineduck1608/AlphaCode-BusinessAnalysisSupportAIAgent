56a7d2ca2affbd8ffdfa5c01584bb66760c9d741
DTNgoc-II0A <dangngoc4332@gmail.com>
2025-11-02 07:55:44 +0700

    Up schema

--- services/mcp_vector/models/db.py
+++ services/mcp_vector/models/db.py
@@ -1,8 +1,13 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
+# load biến môi trường từ file .env
+load_dotenv()
 # URL kết nối
-DATABASE_URL = "postgresql+psycopg2://postgres:123456@localhost:5432/mydb"
+DATABASE_URL = os.getenv('DB_URL')
 
 # Tạo engine
 engine = create_engine(DATABASE_URL, echo=True)  # echo=True để in SQL ra console
